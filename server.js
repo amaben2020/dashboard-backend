86c9c775797c8299d4fbbee285157ae1709cb416
algomachine007 <benneth@bejamas.io>
2022-09-10 12:09:59 +0100

    updating the middleware

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 const jsonServer = require('json-server');
+const jsonServerAuth = require('json-server-auth');
 const server = jsonServer.create();
 const router = jsonServer.router('db.json'); // <== Will be created later
-const middlewares = jsonServer.defaults();
 const port = process.env.PORT || 5000; // <== You can change the port
 
-server.use(middlewares);
+server.use(jsonServerAuth);
 server.use(router);
 
 server.listen(port);
