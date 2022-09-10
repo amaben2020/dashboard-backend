82ff8a5a7055f273a77ca0fef765b484af40e382
algomachine007 <benneth@bejamas.io>
2022-09-10 12:38:45 +0100

    removal of unused quotes

--- server.js
+++ server.js
@@ -4,7 +4,6 @@ const auth = require('json-server-auth');
 const app = jsonServer.create();
 const router = jsonServer.router('db.json');
 
-// You must apply the auth middleware before the router
 app.use(auth);
 app.use(router);
 app.listen(7000);
